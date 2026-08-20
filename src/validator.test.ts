cc8cd724a58c60afe1fcf25d27ed1427aa0a9ae6
RUAN VINICIUS ESTEVAM MELO <ruan.melo007@cs.cruzeirodosul.edu.br>
2026-08-20 11:07:19 -0300

    Codigo que deu certo

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -3,7 +3,7 @@
 import validarChamado from './validator';
 
 describe('Suíte de Testes - Validador de Chamados (Service Desk)', () => {
-    
+
     test('Deve validar com sucesso um chamado com título preenchido', () => {
         const resultado = validarChamado("Erro de conexão com o banco MySQL");
         expect(resultado).toBe(true);
@@ -19,5 +19,17 @@ describe('Suíte de Testes - Validador de Chamados (Service Desk)', () => {
         expect(() => {
             validarChamado("   ");
         }).toThrow("O título do chamado não pode ser vazio.");
+    });
+
+    test('Deve lançar erro se o título possuir menos de 5 caracteres', () => {
+        expect(() => {
+            validarChamado("Erro");
+        }).toThrow("O título do chamado deve possuir no mínimo 5 caracteres.");
+    });
+
+    test('Deve lançar erro se o título possuir mais de 100 caracteres', () => {
+        expect(() => {
+            validarChamado("a".repeat(101));
+        }).toThrow("O título do chamado deve possuir no máximo 100 caracteres.");
     });
 });
--- src/validator.ts
+++ src/validator.ts
@@ -3,8 +3,17 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    const tituloLimpo = titulo.trim();
+
+    if (tituloLimpo.length < 5) {
+        throw new Error("O título do chamado deve possuir no mínimo 5 caracteres.");
+    }
+
+    if (tituloLimpo.length > 100) {
+        throw new Error("O título do chamado deve possuir no máximo 100 caracteres.");
+    }
+
     return true;
 }
 
 export default validarChamado;
-
